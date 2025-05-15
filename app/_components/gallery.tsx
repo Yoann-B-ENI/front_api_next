56f64e4a10990a333c9614e44d6f4fd0fa802987
Yoann-B-ENI <yoann.battu2024@campus-eni.fr>
2025-05-15 09:54:57 +0200

    fix: Image fix + className fix

--- app/_components/gallery.tsx
+++ app/_components/gallery.tsx
@@ -1,9 +1,34 @@
-
+import Image from 'next/image'
 
 export default function Gallery(){
     return (
-        <div>
-            <p>Hello gallery gallery</p>
+    <section className="text-gray-400 bg-gray-900 body-font">
+      <div className="container px-5 py-24 mx-auto">
+        <div className="flex flex-col text-center w-full mb-20">
+          <h1 className="sm:text-3xl text-2xl font-medium title-font mb-4 text-white">Master Cleanse Reliac Heirloom</h1>
+          <p className="lg:w-2/3 mx-auto leading-relaxed text-base">Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical gentrify, subway tile poke farm-to-table. Franzen you probably haven't heard of them man bun deep jianbing selfies heirloom.</p>
         </div>
+
+        <div className="flex flex-wrap -m-4">
+          <div className="lg:w-1/3 sm:w-1/2 p-4">
+            <div className="flex relative">
+              <Image
+                src="https://dummyimage.com/600x360"
+                width={360}
+                height={600}
+                alt="gallery image"
+                className="absolute inset-0 w-full h-full object-cover object-center"
+              />
+              <div className="px-8 py-10 relative z-10 w-full border-4 border-gray-800 bg-gray-900 opacity-0 hover:opacity-100">
+                <h2 className="tracking-widest text-sm title-font font-medium text-blue-400 mb-1">THE SUBTITLE</h2>
+                <h1 className="title-font text-lg font-medium text-white mb-3">Shooting Stars</h1>
+                <p className="leading-relaxed">Photo booth fam kinfolk cold-pressed sriracha leggings jianbing microdosing tousled waistcoat.</p>
+              </div>
+            </div>
+          </div>
+        </div>
+
+      </div>
+    </section>
     );
 }
--- app/_components/navbar.tsx
+++ app/_components/navbar.tsx
@@ -2,20 +2,20 @@ import Link from 'next/link'
 
 export default function NavBar(){
     return (
-        <header class="text-gray-400 bg-gray-900 body-font">
-          <div class="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-            <a class="flex title-font font-medium items-center text-white mb-4 md:mb-0">
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-10 h-10 text-white p-2 bg-blue-500 rounded-full" viewBox="0 0 24 24">
+        <header className="text-gray-400 bg-gray-900 body-font">
+          <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+            <a className="flex title-font font-medium items-center text-white mb-4 md:mb-0">
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-10 h-10 text-white p-2 bg-blue-500 rounded-full" viewBox="0 0 24 24">
                 <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
               </svg>
-              <span class="ml-3 text-xl">React Next Frontend on ArtIoC</span>
+              <span className="ml-3 text-xl">React Next Frontend on ArtIoC</span>
             </a>
-            <nav class="md:ml-auto md:mr-auto flex flex-wrap items-center text-base justify-center">
-              <Link href="/" class="mr-5 hover:text-white">Home</Link>
-              <Link href="/artists" class="mr-5 hover:text-white">Artists</Link>
-              <Link href="/about" class="mr-5 hover:text-white">About</Link>
+            <nav className="md:ml-auto md:mr-auto flex flex-wrap items-center text-base justify-center">
+              <Link href="/" className="mr-5 hover:text-white">Home</Link>
+              <Link href="/artists" className="mr-5 hover:text-white">Artists</Link>
+              <Link href="/about" className="mr-5 hover:text-white">About</Link>
             </nav>
-            <button class="inline-flex items-center bg-gray-800 border-0 py-1 px-3 focus:outline-none hover:bg-gray-700 rounded text-base mt-4 md:mt-0">Button
+            <button className="inline-flex items-center bg-gray-800 border-0 py-1 px-3 focus:outline-none hover:bg-gray-700 rounded text-base mt-4 md:mt-0">Button
               <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-4 h-4 ml-1" viewBox="0 0 24 24">
                 <path d="M5 12h14M12 5l7 7-7 7"></path>
               </svg>
