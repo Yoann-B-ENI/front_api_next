8d425328401f3bd8ef14fe023841f49d975b3f82
Yoann-B-ENI <yoann.battu2024@campus-eni.fr>
2025-05-19 12:09:56 +0200

    refacto: move api calls to service

--- app/(core)/artists/[slug]/page.tsx
+++ app/(core)/artists/[slug]/page.tsx
@@ -1,26 +1,7 @@
 import Gallery from '@components/gallery'
 import { notFound } from 'next/navigation'
+import {fetchOneArtist, fetchAllLazyArtworksByArtist, fetchArtwork} from '@services/api'
 
-
-// for url construction, see https://api.artic.edu/docs/#iiif-image-api
-async function fetchArtwork(id: number): Promise<Artwork |null>{
-    try {
-        const temp = await fetch(`https://api.artic.edu/api/v1/artworks/${id}?fields=id,title,date_display,artist_display,description,image_id`)
-        const {data, config} = await temp.json()
-        const tempImg: Artwork = {
-            id: data.id,
-            title: data.title, 
-            date_display: data.date_display, 
-            artist_display: data.artist_display, 
-            description: data.description, 
-            url: `${config.iiif_url}/${data.image_id}/full/843,/0/default.jpg`
-        }
-        return tempImg
-    } catch (err) {
-        console.error(err)
-        return null;
-    }
-}
 
 export default async function Page({
     params,
@@ -33,19 +14,14 @@ export default async function Page({
     let rawArtworks: Artwork[] = [];
 
     try{
-        const rqArtist = await fetch(`https://api.artic.edu/api/v1/artists/${slug}?fields=title,description`)
-        if (!rqArtist.ok) throw new Error("Artist not found or something went wrong");
-        rawArtist = await rqArtist.json()      //console.log(rawArtist)
+        rawArtist = await fetchOneArtist(slug);
     } catch (err: any){ //TODO catch more precisely
-        console.error(err);
-        // notFound(); // -> next 404 page
+        console.error(err);        // notFound(); // -> next 404 page
         return <div>Artist not found with error: {err.message}</div>; //early return
     }
 
     try{ // should only try if artist was found
-        const rqArtworks = await fetch(`https://api.artic.edu/api/v1/artworks/search?query[term][artist_id]=${slug}&fields=id,title`)
-        if (!rqArtworks.ok) throw new Error("Something went wrong fetching artworks")
-        rawArtworks = (await rqArtworks.json()).data
+        rawArtworks = (await fetchAllLazyArtworksByArtist(slug)).data
         console.log(rawArtworks)
     } catch (err){
         console.error(err)
@@ -60,4 +36,6 @@ export default async function Page({
         <Gallery titleText={"Artist " +slug+ ": " +rawArtist.data.title} description={rawArtist.data.description} imgArray={filteredArtworks}/>
       </div>
     );
+
+
 }
--- app/_services/api.tsx
+++ app/_services/api.tsx
@@ -1,7 +1,42 @@
 
 export async function fetchArtists(page: string): Promise<ArtistsApiResponse> {
-  const baseUrl = process.env.PUBLIC_API_URL
+  const baseUrl = process.env.PUBLIC_API_URL;
   const res = await fetch(`${baseUrl}/artists?page=${page}&fields=id,title,description&limit=9`);
   if (!res.ok) throw new Error('Failed to fetch artists');
   return res.json();
+}
+
+export async function fetchOneArtist(slug: string): Promise<Artist>{
+    const baseUrl = process.env.PUBLIC_API_URL;
+    const rqArtist = await fetch(`${baseUrl}/artists/${slug}?fields=title,description`);
+    if (!rqArtist.ok) throw new Error("Artist not found or something went wrong");
+    return rqArtist.json();
+}
+
+export async function fetchAllLazyArtworksByArtist(slug: string): Promise<Artwork[]>{
+    const baseUrl = process.env.PUBLIC_API_URL;
+    const rqArtworks = await fetch(`${baseUrl}/artworks/search?query[term][artist_id]=${slug}&fields=id,title`)
+    if (!rqArtworks.ok) throw new Error("Something went wrong fetching artworks")
+    return rqArtworks.json()
+}
+
+// for url construction, see https://api.artic.edu/docs/#iiif-image-api
+export async function fetchArtwork(id: number): Promise<Artwork |null>{
+    const baseUrl = process.env.PUBLIC_API_URL;
+    try {
+        const temp = await fetch(`${baseUrl}/artworks/${id}?fields=id,title,date_display,artist_display,description,image_id`)
+        const {data, config} = await temp.json()
+        const tempImg: Artwork = {
+            id: data.id,
+            title: data.title,
+            date_display: data.date_display,
+            artist_display: data.artist_display,
+            description: data.description,
+            url: `${config.iiif_url}/${data.image_id}/full/843,/0/default.jpg`
+        }
+        return tempImg
+    } catch (err) {
+        console.error(err)
+        return null;
+    }
 }
